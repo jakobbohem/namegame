1ea14d75f1fb3c354bf09a834b677d3fb9afee08
Jakob <jakob.bohem@gmail.com>
2014-11-19 00:24:27 +0100

    Added some more example code in the ViewController

--- TipCalculator/ToolViewController.swift
+++ TipCalculator/ToolViewController.swift
@@ -14,24 +14,37 @@ class ToolViewController: UIViewController { // should the viewController also b
     @IBOutlet var textView: UITextView!
     // 'view' implicitly defined here?
     @IBOutlet weak var mainImage: UIImageView!
+    
+    // buttons
+    @IBOutlet weak var buttonTopLeft: UIButton!
+    @IBOutlet weak var buttonBottomLeft: UIButton!
+    @IBOutlet weak var buttonTopRight: UIButton!
+    @IBOutlet weak var buttonBottomRight: UIButton!
+    
     @IBOutlet var imageListener: UITapGestureRecognizer!
-    @IBAction func imageTapped(sender: AnyObject) {
-        print("I got tapped")
-    }
+    
 
 
     // Model connection
     let tipCalc = TipCalculatorModel(total: 100, percent: 0.1)
     var possibleTips = Dictionary<Int, (tip:Double, total:Double)>()
     var sortedKeys:[Int] = []
-    var imageId:String = "" // note: always use init values
-
+    var images:[String] = [] // note: always use init values
+    var friends:[String: String] = Dictionary<String,String>()//Dictionary<name:String: imageUrl:String>()
+    var names: [String] = []
+    var currentImageId = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // save original image id
-        imageId = mainImage.image!.description
-
+        // setup self:
+        images+=["thor.png"]
+        images+=["robert.png"]
+//        
+//        friends.add["Thor Bossoyt", "thor.png"]
+//        friends+= ["Robert Bois", "robert.png"]
+        
+        populateNames()
+        
         refreshUI();
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -40,21 +53,19 @@ class ToolViewController: UIViewController { // should the viewController also b
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-//  OBJ>c
-//    UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapDetected)];
-//    singleTap.numberOfTapsRequired = 1;
-//    [preArrowImage setUserInteractionEnabled:YES];
-//    [preArrowImage addGestureRecognizer:singleTap];
-//    
-//    -(void)tapDetected{
-//    NSLog(@"single Tap on imageview");
-//    
-//    }
-    
     
     func refreshUI() {
         self.textView.text = "Detta är thor!"
-        print("h'r 'r thor")
+        
+        var index = Int(arc4random_uniform(UInt32(names.count)))
+        buttonBottomLeft.setTitle(names[index], forState: UIControlState.Normal)
+        index = Int(arc4random_uniform(UInt32(names.count)))
+        buttonTopLeft.setTitle(names[index], forState: UIControlState.Normal)
+        index = Int(arc4random_uniform(UInt32(names.count)))
+        buttonBottomRight.setTitle(names[index], forState: UIControlState.Normal)
+        index = Int(arc4random_uniform(UInt32(names.count)))
+        buttonTopRight.setTitle(names[index], forState: UIControlState.Normal)
+        
         // set up image click listener
         self.imageListener.numberOfTapsRequired = 1;
         
@@ -63,15 +74,33 @@ class ToolViewController: UIViewController { // should the viewController also b
     // UI Actions
     @IBAction func nameTopRight(sender: AnyObject) {
         //        debugInfo =
-        print("replacing image \(mainImage.image)")
+        println("replacing image \(mainImage.image)")
         // replace image
-        if(mainImage.image?.description == self.imageId) {
-            mainImage.image = UIImage(named: "robert.png")
-        }
-        else {
-            mainImage.image = UIImage(named: "thor.png")
-        }
+        cycleImage()
         
+        
+    }
+    @IBAction func imageTapped(sender: AnyObject) {
+        println("I got tapped")
+    }
+    
+    @IBAction func clickedName(sender: UIButton) {
+        println("clicked name: \(sender.titleLabel!.text)")
+        
+    }
+    
+    // Helper methods:
+    func cycleImage() {
+        currentImageId+=1
+        currentImageId = currentImageId % images.count
+        
+        println("image id = \(currentImageId)");
+        mainImage.image = UIImage(named: images[currentImageId]);
+        
+    }
+    
+    func populateNames() {
+        self.names += ["Thor Bossuyt", "Jakob Ryden", "Robert Bois", "Anna Svensson", "Stefan Carlsson", "Ivan Milles", "Marie Fast", "Karin af Klintberg", "Bobbo Memme"]
     }
     
 }
